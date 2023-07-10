939dc9d06987f32d442d6a970a1f8e82a33e3605
irvinakocan <44883640+irvinakocan@users.noreply.github.com>
2023-07-11 01:11:26 +0200

    Implementirano rješenje

--- Lab3_z3/Program.cs
+++ Lab3_z3/Program.cs
@@ -1,3 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+namespace Lab3_z3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite brojeve (razdvojene razmakom):");
+            string unos = Console.ReadLine();
+            string[] pojedinacniUnosi = unos.Split(" ");
+
+            List<int> brojevi = new List<int>();
+            foreach (var s in pojedinacniUnosi)
+                brojevi.Add(Int32.Parse(s));
+
+
+            var parni = brojevi.Where(broj => broj % 2 == 0).ToList();
+            var neparni = brojevi.Where(broj => broj % 2 != 0).ToList();
+
+            List<int> redukovaniBrojevi;
+            if (parni.Count > neparni.Count)
+                redukovaniBrojevi = parni;
+            else
+                redukovaniBrojevi = neparni;
+
+            int suma = 0;
+            redukovaniBrojevi.ForEach(broj => suma += broj);
+
+            if (suma < 0)
+                Console.WriteLine("Greška! Dobili ste sumu: {0}", suma);
+            else
+            {
+                Console.Write("Elementi: ");
+                foreach (var broj in redukovaniBrojevi)
+                    Console.Write("{0} ", broj);
+                Console.Write("\nSuma: {0}", suma);
+            }
+        }
+    }
+}
